0619ddb9ae7a96da921a1604e834cc4618e78200
vanthao03596 <phamthao03596@gmail.com>
2020-11-27 14:09:21 +0700

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('Spatie\Mailcoach\Http\App\Queries\CampaignsQuery', 'App\Queries\CampaignsQuery');
-        $this->app->bind('Spatie\Mailcoach\Actions\Campaigns\UpdateCampaignAction', 'Spatie\Mailcoach\Http\Actions\UpdateCampaignAction');
+        $this->app->bind('Spatie\Mailcoach\Actions\Campaigns\UpdateCampaignAction', 'Spatie\Mailcoach\Actions\Campaigns\UpdateCampaignAction');
         // $this->app->when(CampaignsController::class)
         //   ->needs(CampaignsQuery::class)
         //   ->give(\App\Queries\CampaignsQuery::class);
